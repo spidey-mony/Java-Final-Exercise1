a47f795d94bd0140b612b11000e8eb3f42924df3
monymocki <110289642+spidey-mony@users.noreply.github.com>
2024-02-22 08:19:57 +0700

    Final Exam

--- Exercise1/NumberLists.java
+++ Exercise1/NumberLists.java
@@ -2,21 +2,26 @@ package Exercise1;
 import java.util.Scanner;
 
 public class NumberLists {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the number of elements: ");
-        int num = input.nextInt();
+        System.out.println("Enter numbers(show output by enter non-integer value): ");
 
-        int[] numbers = new int[num];
+        int[] numbers = new int[1];
+        int size = 0;
 
-        System.out.print("Enter the numbers: ");
-        for (int i=0; i < num; i++){
-            numbers[i] = input.nextInt();
+        while (input.hasNextInt()) {
+            if (size == numbers.length) {
+
+                int[] newArray = new int[numbers.length * 2];
+                System.arraycopy(numbers, 0, newArray, 0, numbers.length);
+                numbers = newArray;
+            }
+            numbers[size++] = input.nextInt();
         }
 
-        System.out.println("The input numbers are: ");
-        for (int i=0; i < num; i++){
+        System.out.println("The numbers that you inputted are: ");
+        for (int i = 0; i < size; i++) {
             System.out.print(numbers[i] + " ");
         }
 
